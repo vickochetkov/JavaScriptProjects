a175e7bcb7556f685525fc6e92d580abf903e378
Victor <vickochetkov@gmail.com>
2018-07-04 13:19:52 -0700

    Added solution 05_CashRegister.js

--- 05_CashRegister.js
+++ 05_CashRegister.js
@@ -7,32 +7,40 @@
 // Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
 
 function checkCashRegister(price, cash, cid) {
-  var change;
-  // Here is your change, ma'am.
+  var change = {status: "OPEN", change: []},
+	par = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100],
+	drwrSum = cid.reduce((sum, b) => sum + b[1], 0).toFixed(2),
+	chDue = cash - price;
+
+	if (Number(drwrSum) < Number(chDue)) {
+		return {status: "INSUFFICIENT_FUNDS", change: []};
+	} else if (Number(drwrSum) === Number(chDue)) {
+		return {status: "CLOSED", change: cid};
+	}
+	for (var i = par.length - 1; i >= 0; i--) {
+		let chngArr = [cid[i][0], 0];
+		while (chDue >= par[i] && cid[i][1]) {
+			chngArr[1] += par[i];
+			chDue = (chDue - par[i]).toFixed(2);
+			cid[i][1] -= par[i];
+		}
+		if (chngArr[1] !== 0) change.change.push(chngArr);
+	}
+	if (chDue > 0) return {status: "INSUFFICIENT_FUNDS", change: []};
   return change;
 }
 
-// Example cash-in-drawer array:
-// [["PENNY", 1.01],
-// ["NICKEL", 2.05],
-// ["DIME", 3.1],
-// ["QUARTER", 4.25],
-// ["ONE", 90],
-// ["FIVE", 55],
-// ["TEN", 20],
-// ["TWENTY", 60],
-// ["ONE HUNDRED", 100]]
-
-
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
-//ooo
 console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
 // should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+
 console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
 // should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+
 console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
 // should return {status: "INSUFFICIENT_FUNDS", change: []}.
+
 console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
 // should return {status: "INSUFFICIENT_FUNDS", change: []}.
+
 console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
 // should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
